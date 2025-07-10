bb06039b3dc0ec86bc92c54cd219bb19428ebbb4
anika-gautam <anika.vgautam@gmail.com>
2025-07-10 12:50:08 -0700

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,24 @@
+x=10
+y=12
+z=0
+if x and y and z:
+   print("All numbers are true")
+else:
+   print("At least one number is false")
+x=10
+y=-10
+z=0
+if x>0 or y>0:
+  print("Either number is greater than 0 ")
+else:
+     print("None of the numbers are greater than 0")
+if y>0 or z>0:
+   print("Either number is greater than 0 ")  
+else:
+    print("None of the numbers are greater than 0 ")   
+a=0
+if not a:   
+   print("True") 
+else:
+    print("False")
+   
