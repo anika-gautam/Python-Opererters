69a641a55c89808230666b8a8511be06c27919dc
anika-gautam <anika.vgautam@gmail.com>
2025-07-17 12:38:42 -0700

    Add files via upload

---
+++ Challenge1.py
@@ -0,0 +1,14 @@
+x=4
+y=7
+z=5
+a=8
+b=10
+c=(x+z)**a*b
+print("The value of (x+z)**a/b, is ", c)
+name="Jack"
+age=0
+if name=="Jack" or name== "Bob" and age>=5:
+    print("Hi")
+else:
+    print("Bye")
+
